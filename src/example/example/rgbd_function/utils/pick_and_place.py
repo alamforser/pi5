31094c8ee6e4e1ed59b107a0e5db09c7afdf0c29
alamforser <alamforser@gmail.com>
2025-06-09 18:53:50 -0700

    Update pick_and_place.py

--- src/example/example/rgbd_function/utils/pick_and_place.py
+++ src/example/example/rgbd_function/utils/pick_and_place.py
@@ -10,7 +10,7 @@ from kinematics.kinematics_control import set_pose_target
 from servo_controller.bus_servo_control import set_servo_position
 
 stop = False
-chassis_type = os.environ['CHASSIS_TYPE']
+chassis_type = os.environ['MACHINE_TYPE']
 
 def interrupt(status=False):
     global stop
@@ -33,25 +33,25 @@ def pick_without_back(position, pitch, yaw, gripper_angle, gripper_depth, joints
             msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0, duration=0.1)
             res = send_request(kinematics_client, msg)
             servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
-            set_servo_position(joints_pub, 1.0, ((6, servo_data[-1][0]),))
+            set_servo_position(joints_pub, 1.0, ((1, servo_data[-1][0]),))
             
             msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0, duration=1.5)
             res = send_request(kinematics_client, msg)
             servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
             for i in servo_data:
-                set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                 time.sleep(dt - d)
         else:
             msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0)
             res = send_request(kinematics_client, msg)
             servo_data = res.pulse
-            set_servo_position(joints_pub, 1.0, ((6, servo_data[0]),))
+            set_servo_position(joints_pub, 1.0, ((1, servo_data[0]),))
             time.sleep(1)
 
-            set_servo_position(joints_pub, 1, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+            set_servo_position(joints_pub, 1, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
             time.sleep(1)
 
-        set_servo_position(joints_pub, 0.5, ((2, yaw),))
+        set_servo_position(joints_pub, 0.5, ((5, yaw),))
         time.sleep(0.5)
         
         if not stop:
@@ -62,16 +62,16 @@ def pick_without_back(position, pitch, yaw, gripper_angle, gripper_depth, joints
                 res = send_request(kinematics_client, msg)
                 servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
                 for i in servo_data:
-                    set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                    set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                     time.sleep(dt - d)
             else:
                 msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0)
                 res = send_request(kinematics_client, msg)
                 servo_data = res.pulse
-                set_servo_position(joints_pub, 0.5, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+                set_servo_position(joints_pub, 0.5, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
                 time.sleep(0.5)
 
-            set_servo_position(joints_pub, 0.5, ((1, gripper_angle),))
+            set_servo_position(joints_pub, 0.5, ((10, gripper_angle),))
             time.sleep(0.5)
 
             if not stop:
@@ -82,13 +82,13 @@ def pick_without_back(position, pitch, yaw, gripper_angle, gripper_depth, joints
                     res = send_request(kinematics_client, msg)
                     servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
                     for i in servo_data:
-                        set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                        set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                         time.sleep(dt - d)
                 else:
                     msg = set_pose_target(position, pitch, [-180.0, 180.0], 1.0)
                     res = send_request(kinematics_client, msg)
                     servo_data = res.pulse
-                    set_servo_position(joints_pub, 0.5, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+                    set_servo_position(joints_pub, 0.5, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
                     time.sleep(0.5)
                 return True
     return False
@@ -105,10 +105,10 @@ def pick(position, pitch, yaw, gripper_angle, gripper_depth, joints_pub, kinemat
                     msg = set_pose_target([0.11, 0.0, 0.09], 73, [-180.0, 180.0], 1.0, duration=1.0)
                 res = send_request(kinematics_client, msg)
                 servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
-                set_servo_position(joints_pub, 0.5, ((2, 500),))
+                set_servo_position(joints_pub, 0.5, ((5, 500),))
                 time.sleep(0.02)
                 for i in servo_data:
-                    set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                    set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                     time.sleep(dt - d)
             else:
                 if chassis_type == 'Slide_Rails':
@@ -117,9 +117,9 @@ def pick(position, pitch, yaw, gripper_angle, gripper_depth, joints_pub, kinemat
                     msg = set_pose_target([0.11, 0.0, 0.09], 73, [-180.0, 180.0], 1.0)
                 res = send_request(kinematics_client, msg)
                 servo_data = res.pulse
-                set_servo_position(joints_pub, 0.5, ((2, 500),))
+                set_servo_position(joints_pub, 0.5, ((5, 500),))
                 time.sleep(0.02)
-                set_servo_position(joints_pub, 1, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+                set_servo_position(joints_pub, 1, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
                 time.sleep(1)
             return True
     return False
@@ -140,17 +140,17 @@ def place(position, pitch, yaw, gripper_angle, joints_pub, kinematics_client, in
             res = send_request(kinematics_client, msg)
             servo_data1 = np.array(res.pulse).reshape(-1, 5).tolist()
             for i in servo_data1:
-                set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                 time.sleep(dt - d)
         else:
             msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0)
             res = send_request(kinematics_client, msg)
             servo_data = res.pulse
-            set_servo_position(joints_pub, 1.0, ((6, servo_data[0]),))
+            set_servo_position(joints_pub, 1.0, ((1, servo_data[0]),))
             time.sleep(1)
             print("11")
             # self.get_logger().info(f'222:{position}')
-            set_servo_position(joints_pub, 1, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+            set_servo_position(joints_pub, 1, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
             print("22")
             # self.get_logger().info(f'333:{position}')
             time.sleep(1)
@@ -158,7 +158,7 @@ def place(position, pitch, yaw, gripper_angle, joints_pub, kinematics_client, in
         if not stop:
             position[2] -= 0.03
             
-            set_servo_position(joints_pub, 0.5, ((2, yaw),))
+            set_servo_position(joints_pub, 0.5, ((5, yaw),))
             time.sleep(0.8)
             
             if interpolation:
@@ -166,16 +166,16 @@ def place(position, pitch, yaw, gripper_angle, joints_pub, kinematics_client, in
                 res = send_request(kinematics_client, msg)
                 servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
                 for i in servo_data:
-                    set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                    set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                     time.sleep(dt - d)
             else:
                 msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0)
                 res = send_request(kinematics_client, msg)
                 servo_data = res.pulse
-                set_servo_position(joints_pub, 1, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+                set_servo_position(joints_pub, 1, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
                 time.sleep(1.2)
 
-            set_servo_position(joints_pub, 0.5, ((1, gripper_angle),))
+            set_servo_position(joints_pub, 0.5, ((10, gripper_angle),))
             time.sleep(0.5)
             
             position[2] += 0.03
@@ -184,15 +184,15 @@ def place(position, pitch, yaw, gripper_angle, joints_pub, kinematics_client, in
                 res = send_request(kinematics_client, msg)
                 servo_data = np.array(res.pulse).reshape(-1, 5).tolist()
                 for i in servo_data:
-                    set_servo_position(joints_pub, dt, ((5, i[1]), (4, i[2]), (3, i[3])))
+                    set_servo_position(joints_pub, dt, ((2, i[1]), (3, i[2]), (4, i[3])))
                     time.sleep(dt - d)          
             else:
                 msg = set_pose_target(position, pitch, [-90.0, 90.0], 1.0)
                 res = send_request(kinematics_client, msg)
                 servo_data = res.pulse
-                set_servo_position(joints_pub, 0.5, ((5, servo_data[1]), (4, servo_data[2]), (3, servo_data[3])))
+                set_servo_position(joints_pub, 0.5, ((2, servo_data[1]), (3, servo_data[2]), (4, servo_data[3])))
                 time.sleep(0.5)
-            set_servo_position(joints_pub, 0.5, ((1, 200),))
+            set_servo_position(joints_pub, 0.5, ((10, 200),))
             return True
     return False
 
